5c5465db1e4f9529eca4e6945db1f059f8bfab39
apollo-user-1 <67097752+jd-d@users.noreply.github.com>
2026-01-28 17:00:44 +0100

    added meanings for Latin perfectums

--- stories/oudgrieks-vocab-test/vocab-latin-nl.js
+++ stories/oudgrieks-vocab-test/vocab-latin-nl.js
@@ -8,8 +8,8 @@ window.VOCAB_DATASETS.push({
   data: `
 triclinium	eetkamer
 condere	stichten, verbergen
-condidi	pf van condere
-cupivi	pf van cupere
+condidi	pf van condere, gesticht, verborgen
+cupivi	pf van cupere, graag gewild, verlangd
 dolor, dolores m	pijn, verdriet
 donec	totdat
 dubitare	aarzelen, twijfelen
@@ -17,34 +17,34 @@ imago, imagines v	beeld, afbeelding
 inquit	hij/zij zegt, hij/zij zei
 iuvenis, iuvenes m	jongeman
 mollis, -e	zacht
-movi	pf van movere
+movi	pf van movere, bewogen
 mutare	veranderen
 nullus, -a, -um	geen
 ob + acc	wegens
 punire	straffen
 qui, quae, quod	welke, die, dat
 repetere	herhalen
-repetivi	pf van repetere
-sensi	pf van sentire
+repetivi	pf van repetere, herhaald
+sensi	pf van sentire, gemerkt, gevoeld, gemeend
 sentire	merken, voelen, menen
 spernere	afwijzen
-sprevi	pf van spernere
+sprevi	pf van spernere, afgewezen
 superbia	trots, arrogantie
 ultimus, -a, -um	laatste, uiterste
 ultro	uit zichzelf
 fullonia	vollerij
 accidere	gebeuren
-addidi	pf van addere
+addidi	pf van addere, toegevoegd
 alius … alius	de een … de ander
 canere	zingen
 carmen, carmina	lied, gedicht
 certus, -a, -um	zeker, vast
 colligere	verzamelen
 fundere	gieten, storten
-intellexi	pf van intellegere
+intellexi	pf van intellegere, begrepen
 labor, labores m	werk, inspanning
 laedere	beledigen, kwetsen
-laesi	pf van laedere
+laesi	pf van laedere, beledigd, gekwetst
 lavare	wassen
 neque … neque	niet … en ook niet
 olim	vroeger, eens
@@ -52,15 +52,15 @@ perficere, perficio	voltooien
 plenus, -a, -um (+ gen)	vol (met)
 primus, -a, -um	eerste
 se	zichzelf, dat hij, dat zij
-vendidi	pf van vendere
+vendidi	pf van vendere, verkocht
 pistrina	bakkerij
 amplus, -a, -um	ruim, groot, belangrijk
 animal, animalia	dier
 censēre	van mening zijn
-commisi	pf van committere
+commisi	pf van committere, toevertrouwd
 committere	toevertrouwen
 egēre + abl	nodig hebben
-egi	pf van agere
+egi	pf van agere, geleid, gevoerd, gedaan
 finis, fines m	grens, einde, gebied
 frumentum	graan
 ignotus, -a, -um	onbekend
@@ -69,9 +69,9 @@ meminisse pf	zich herinneren dat, zich herinneren
 officium	plicht, taak
 orbis, orbes m	kring, cirkel
 perpetuus, -a, -um	voortdurend
-perveni	pf van pervenire
+perveni	pf van pervenire, aangekomen
 pervenire	aankomen
-praeterii	pf van praeterire
+praeterii	pf van praeterire, voorbijgegaan
 praeterire	voorbijgaan
 premere	drukken (op)
 procul	ver, in de verte
