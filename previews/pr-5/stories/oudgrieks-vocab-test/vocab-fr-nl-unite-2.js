52bc52169dc0cf48b08a3627a0bad4de7fe25202
jd-d <jd-d@users.noreply.github.com>
2026-02-03 13:22:41 +0000

    deploy: 8101dfa562f03305c245c72331b04cacf17a9157

--- previews/pr-5/stories/oudgrieks-vocab-test/vocab-fr-nl-unite-2.js
+++ previews/pr-5/stories/oudgrieks-vocab-test/vocab-fr-nl-unite-2.js
@@ -1,7 +1,7 @@
 window.VOCAB_DATASETS = window.VOCAB_DATASETS || [];
 window.VOCAB_DATASETS.push({
-  id: "fr-nl-unite-2-3",
-  label: "Frans → Nederlands (Unité 2-3)",
+  id: "fr-nl-unite-2",
+  label: "Frans → Nederlands (Unité 2)",
   language1: "Frans",
   language2: "Nederlands",
   showTransliteration: false,
@@ -94,56 +94,5 @@ il fait (très) beau	het is (heel) mooi weer
 nuageux	bewolkt
 il pleut (beaucoup)	het regent (veel)
 il neige (beaucoup)	het sneeuwt (veel)
-l'écran (m.)	het beeldscherm
-quelqu'un	iemand
-l'outil (m.)	het hulpmiddel, de tool
-imprimer	printen, drukken
-ça m'est égal	het maakt me niet uit
-la préférence	de voorkeur
-recevoir	ontvangen
-la lettre	de brief
-le mail	de mail
-taper	typen
-la revue, le magazine	het tijdschrift
-je préfère	ik heb liever, ik geef de voorkeur aan
-ou	of
-avec	met
-sans	zonder
-Quoi ?	Wat?
-Qu'est-ce que ?	Wat?
-Où ?	Waar?
-Comment ?	Hoe?
-Combien ?	Hoeveel?
-Quand ?	Wanneer?
-Qui ?	Wie?
-Pourquoi ?	Waarom?
-Quel, quelle, quels, quelles ?	Welke?
-le jeune	de jongen
-partout	overal
-surtout	vooral
-le journal	de krant
-le problème	het probleem
-le téléphone fixe	de vaste telefoon
-plusieurs	verscheidene
-tous, toutes	alle
-même	zelfs
-Je suis d'accord avec...	Ik ben het eens met...
-aussi	ook
-gratuit	gratis
-essayer	proberen
-savoir	weten, kunnen
-pouvoir	kunnen
-connaître	kennen
-l'opinion (v.)	de mening
-l'avis (m.)	de mening
-haïr	haten
-C'est génial	Het is geweldig
-Je ne sais pas	Ik weet het niet
-Je pense que...	Ik denk dat...
-Je trouve que...	Ik vind dat...
-nul	waardeloos
-les bons côtés	de goede kanten
-les mauvais côtés	de slechte kanten
-Pour moi	Volgens mij
 `
 });
