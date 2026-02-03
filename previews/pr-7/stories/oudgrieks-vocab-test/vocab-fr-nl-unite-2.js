93fa296870841e9c37587f18fef80f5935465b40
jd-d <jd-d@users.noreply.github.com>
2026-02-03 16:54:43 +0000

    deploy: c65b5c80c7a4bc02f7aa7fb0304bba7fe649615d

---
+++ previews/pr-7/stories/oudgrieks-vocab-test/vocab-fr-nl-unite-2.js
@@ -0,0 +1,98 @@
+window.VOCAB_DATASETS = window.VOCAB_DATASETS || [];
+window.VOCAB_DATASETS.push({
+  id: "fr-nl-unite-2",
+  label: "Frans → Nederlands (Unité 2)",
+  language1: "Frans",
+  language2: "Nederlands",
+  showTransliteration: false,
+  data: `
+en ville	in de stad
+la place	het plein
+l'arbre (m.)	de boom
+la poubelle	de prullenbak
+le chien	de hond
+l'appareil photo (m.)	het fototoestel
+le vélo	de fiets
+le touriste	de toerist
+le café	het café
+à gauche de	links van
+derrière	achter
+devant	vóór
+sur	op
+sous	onder
+au milieu de	in het midden van
+à droite de	rechts van
+en face de	tegenover
+entre	tussen
+à côté	naast
+dans	in
+l'objet (m.)	het voorwerp
+lourd, lourde	zwaar
+les vacances (v. mv.)	de vakantie
+la terre	de aarde
+autour de	rondom
+le cou	de nek
+il fait froid	het is koud
+loin de	ver van
+près de	dichtbij
+en route	onderweg
+le voyage	de reis
+le tour	de toer, de rondreis
+gagner	winnen
+voyager	reizen
+découvrir	ontdekken
+l'avion (m.)	het vliegtuig
+durer	duren
+visiter	bezoeken
+l'arrivée (v.)	de aankomst
+après	na, daarna
+la visite	het bezoek
+le château	het kasteel
+le train	de trein
+le poisson	de vis
+la fois	de keer
+toujours	altijd, nog altijd
+continuer	vervolgen, doorgaan
+en bus	met de bus
+vers	naar
+monter	naar boven gaan
+comment	hoe
+chaque	ieder
+la carte postale	de ansichtkaart
+raconter	vertellen
+ensuite	vervolgens
+le bateau	de boot
+en métro	met de metro
+en voiture	met de auto
+en avion	met het vliegtuig
+en skate	met het skateboard
+à moto	met de motor
+à vélo	met de fiets
+à pied	te voet, lopend
+qui	die, dat
+en vacances	op vakantie
+le climat	het klimaat
+l'île (v.)	het eiland
+la plage	het strand
+jusque, jusqu'	tot
+30 degrés (m.)	30 graden
+le soleil	de zon
+le paysage	het landschap
+magnifique	geweldig
+l'habitant (m.), l'habitante (v.)	de inwoner
+nager	zwemmen
+gros bisous	dikke kus
+arriver	aankomen
+toute la journée	de hele dag
+rester	blijven
+la piscine	het zwembad
+la montagne	de berg
+la dernière fois	de laatste keer, de vorige keer
+il fait (très) chaud	het is (heel) warm
+il fait (très) froid	het is (heel) koud
+il fait (très) beau	het is (heel) mooi weer
+nuageux	bewolkt
+il pleut (beaucoup)	het regent (veel)
+il neige (beaucoup)	het sneeuwt (veel)
+`
+});
