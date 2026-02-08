80fd1b160792bb75b3fa0138fbecb16c8d10e092
apollo-user-1 <67097752+jd-d@users.noreply.github.com>
2026-02-08 20:00:41 +0100

    Add multi-select functionality for vocabtrainer datasets, add 2 latin sets, improve UI elements/ flicker

---
+++ stories/vocabtrainer/vocab-latin-nl-les-27-thermae.js
@@ -0,0 +1,33 @@
+window.VOCAB_DATASETS = window.VOCAB_DATASETS || [];
+window.VOCAB_DATASETS.push({
+  id: "lat-nl-les-27-thermae",
+  label: "Latijn -> Nederlands (Les 27, Thermae)",
+  language1: "Latijn",
+  language2: "Nederlands",
+  showTransliteration: false,
+  data: `
+thermae	badgebouw, thermen
+accidit	pf van accidere
+anxius, -a, -um	angstig, bezorgd
+coegi	pf van cogere
+cogere	bijeenbrengen, dwingen
+delere	vernielen
+frequens, frequent-	herhaaldelijk
+Iovis	gen van Jupiter
+necesse est	het is nodig
+nescire	niet weten
+nonnulli	sommige, sommigen
+perfeci	pf van perficere
+post	achter, na, later
+quisque	ieder, elk
+rapere, rapio	roven, grijpen
+rapui	pf van rapere
+regio, regiones	streek, gebied
+ruere	zich haasten, neerstorten, instorten
+rui	pf van ruere
+tremere	trillen, beven
+unde	waarvandaan
+vehemens, vehement-	hevig, krachtig
+vetus, veter-	oud
+`
+});
