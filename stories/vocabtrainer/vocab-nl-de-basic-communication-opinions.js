104122fecad1c845b646e474bbee996bfa3aaf42
apollo-user-1 <67097752+jd-d@users.noreply.github.com>
2026-03-05 23:13:55 +0100

    Add Dutch to German vocab datasets

---
+++ stories/vocabtrainer/vocab-nl-de-basic-communication-opinions.js
@@ -0,0 +1,22 @@
+window.VOCAB_DATASETS = window.VOCAB_DATASETS || [];
+window.VOCAB_DATASETS.push({
+  id: "nl-de-basic-communication-opinions",
+  label: "Nederlands → Duits (Basic Communication + Opinions)",
+  language1: "Nederlands",
+  language2: "Duits",
+  showTransliteration: false,
+  data: `
+met	mit
+of	oder
+soms	manchmal
+bevallen	gefallen, gefallen
+leuk zijn	Spaß machen
+dom, stom	blöd
+interessant	interessant
+leuk	toll
+makkelijk	leicht
+moeilijk	schwierig
+mooi	schön
+saai	langweilig
+`
+});
---
+++ stories/vocabtrainer/vocab-nl-de-hobbies-free-time.js
@@ -0,0 +1,33 @@
+window.VOCAB_DATASETS = window.VOCAB_DATASETS || [];
+window.VOCAB_DATASETS.push({
+  id: "nl-de-hobbies-free-time",
+  label: "Nederlands → Duits (Hobbies & Free Time)",
+  language1: "Nederlands",
+  language2: "Duits",
+  showTransliteration: false,
+  data: `
+de gitaar	die Gitarre, die Gitarren
+de stad	die Stadt, die Städte
+de viool	die Geige, die Geigen
+het drumstel	das Schlagzeug, die Schlagzeuge
+de hobby	das Hobby, die Hobbys
+de piano	das Klavier, die Klaviere
+de vakantie	die Ferien
+aan sport doen	Sport machen
+dansen	tanzen
+fietsen	Rad fahren, gefahren
+gamen	gamen
+lezen	lesen, gelesen
+naar de bioscoop gaan	ins Kino gehen, gegangen
+naar het museum gaan	ins Museum gehen
+naar het zwembad gaan	ins Schwimmbad gehen
+naar muziek luisteren	Musik hören
+paardrijden	reiten, geritten
+skiën	Ski fahren, gefahren
+spelen	spielen
+tennissen	Tennis spielen
+voetballen	Fußball spielen
+zingen	singen, gesungen
+zwemmen	schwimmen, geschwommen
+`
+});
